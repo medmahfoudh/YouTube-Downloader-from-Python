e0b646adfd4f3c5d041bc59d0b920b028e1ed620
Mohamed Mahfoudh Bouna <medmahfoudhbouna@gmail.com>
2023-01-24 14:24:32 +0100

    Title and config window

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from pytube import YouTube
 from tkinter import *
 
+
+window = Tk()
+window.config(padx=30 , pady=30)
+window.title("YouTube Downloader")
+
 def Download(link):
     youtubeObject = YouTube(link)
     youtubeObject = youtubeObject.streams.get_highest_resolution()
