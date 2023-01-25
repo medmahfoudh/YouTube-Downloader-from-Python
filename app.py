96adc73bdba46bb29ff15806dcd00c3bdaa031a4
Mohamed Mahfoudh Bouna <medmahfoudhbouna@gmail.com>
2023-01-25 18:16:24 +0100

    Function For Download

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ window = Tk()
 window.config(padx=30 , pady=30)
 window.title("YouTube Downloader")
 
-link_label=Label(text='Note DS1 math:',font=('Courrier',12,'bold'))
+link_label=Label(text='Link:',font=('Courrier',12,'bold'))
 link_label.grid(column=0, row=0)
 link_label.config(padx=20,pady=20)
 input_link_label = Entry()
 input_link_label.grid(column=0 , row=1)
 
 def Download(link):
+    link = input_link_label.get()
     youtubeObject = YouTube(link)
     youtubeObject = youtubeObject.streams.get_highest_resolution()
     try:
