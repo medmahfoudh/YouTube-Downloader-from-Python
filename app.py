2b576f2ed92158489b11c211a9c56fcbdb7b5aa3
Mohamed Mahfoudh Bouna <medmahfoudhbouna@gmail.com>
2023-01-26 14:51:44 +0100

    Button Command

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def Download(link):
     print("Download is completed successfully")
 
 
-button_Download=Button(text='' , command=moyenne_tous_les_matieres)
+button_Download=Button(text='click to download' , command=Download)
 
-link = input("Enter the YouTube video URL: ")
-Download(link)
+# link = input("Enter the YouTube video URL: ")
+# Download(link)
